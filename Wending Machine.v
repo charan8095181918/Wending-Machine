47a2adbee55837af1bd084d0360df4f1b95cd068
charan8095181918 <60030130+charan8095181918@users.noreply.github.com>
2022-03-21 00:15:54 +0530

    Add files via upload

---
+++ Wending Machine.v
@@ -0,0 +1,7 @@
+//The problem is to design a practical control circuit for an automatic wending machine let us assume that this machine only accepts 5 rupees(n) 10 rupees(d)  and 20 rupees(q), and keeps count of current money total other inputs to this circuit are refund button and two item choices rupees 55 for soda  and Rupees 70 for water.It is assumed that only one input signal is presnet at a given time the output of the circuit are
+//a).Total
+//b).return Point
+//c).Eject water
+//d).Eject soda
+//e).Eject change
+//The beep signal is asserted if the item choice is entered  before sufficient money has been input,return coin signal is asserted  to return  the most recent coin entry if the addition of this coin will cause  2 overflow the maximum money total that can be maintained ,eject change signal is aserted if more is input that can be stored in the total register.
